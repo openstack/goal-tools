31a85eadd34e0b696af7ed351bc3dd28a80bc4d7
Doug Hellmann <doug@doughellmann.com>
2018-04-29 21:41:20 -0400

    remove unused code from the governance module
    Change-Id: I61bc68f70554b4c1f9d244b24bfb790f2b08440d
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- goal_tools/governance.py
+++ goal_tools/governance.py
@@ -13,8 +13,6 @@
 """Work with the governance repository.
 """
 
-import weakref
-
 import yaml
 
 from goal_tools import apis
@@ -34,17 +32,6 @@ def get_team_data(url=PROJECTS_LIST):
     return yaml.load(r.text)
 
 
-def get_tags_for_deliverable(team_data, team, name):
-    "Return the tags for the deliverable owned by the team."
-    if team not in team_data:
-        return set()
-    team_info = team_data[team]
-    dinfo = team_info['deliverables'].get(name)
-    if not dinfo:
-        return set()
-    return set(dinfo.get('tags', [])).union(set(team_info.get('tags', [])))
-
-
 def get_repo_owner(team_data, repo_name):
     """Return the name of the team that owns the repository.
 
@@ -57,112 +44,3 @@ def get_repo_owner(team_data, repo_name):
             if repo_name in dinfo.get('repos', []):
                 return team
     raise ValueError('Repository %s not found in governance list' % repo_name)
-
-
-class Team(object):
-    _liaison_data = None
-
-    def __init__(self, name, data):
-        self.name = name
-        self.data = data
-        # Protectively initialize the ptl data structure in case part
-        # of it is missing from the project list, then replace any
-        # values we do have from that data.
-        self.ptl = {
-            'name': 'MISSING',
-            'irc': 'MISSING',
-        }
-        self.ptl.update(data.get('ptl', {}))
-        self.deliverables = {
-            dn: Deliverable(dn, di, self)
-            for dn, di in self.data.get('deliverables', {}).items()
-        }
-
-    @property
-    def tags(self):
-        return set(self.data.get('tags', []))
-
-
-class Deliverable(object):
-    def __init__(self, name, data, team):
-        self.name = name
-        self.data = data
-        self.team = weakref.proxy(team)
-        self.repositories = {
-            rn: Repository(rn, self)
-            for rn in self.data.get('repos', [])
-        }
-
-    @property
-    def type(self):
-        for t in self.tags:
-            if t.startswith('type:'):
-                return t.partition(':')[-1]
-        return 'other'
-
-    @property
-    def model(self):
-        for t in self.tags:
-            if t.startswith('release:'):
-                return t.partition(':')[-1]
-        return 'none'
-
-    @property
-    def tags(self):
-        return set(self.data.get('tags', [])).union(self.team.tags)
-
-
-class Repository(object):
-    def __init__(self, name, deliverable):
-        self.name = name
-        self.deliverable = weakref.proxy(deliverable)
-
-    @property
-    def tags(self):
-        return self.deliverable.tags
-
-    @property
-    def code_related(self):
-        return not (self.name.endswith('-specs') or
-                    'cookiecutter' in self.name)
-
-
-def get_repositories(team_data, team_name=None, deliverable_name=None,
-                     tags=[], code_only=False):
-    """Return a sequence of repositories, possibly filtered.
-
-    :param team_data: The result of calling :func:`get_team_data`
-    :param team_name: The name of the team owning the repositories. Can be
-        None.
-    :para deliverable_name: The name of the deliverable to which all
-       repos should belong.
-    :param tags: The names of any tags the repositories should
-        have. Can be empty.
-    :param code_only: Boolean indicating whether to return only code
-      repositories (ignoring specs and cookiecutter templates).
-
-    """
-    if tags:
-        tags = set(tags)
-    if team_name:
-        try:
-            teams = [Team(team_name, team_data[team_name])]
-        except KeyError:
-            raise RuntimeError('No team %r found in %r' %
-                               (team_name, list(team_data.keys())))
-    else:
-        teams = [Team(n, i) for n, i in team_data.items()]
-    for team in teams:
-        if deliverable_name and deliverable_name not in team.deliverables:
-            continue
-        if deliverable_name:
-            deliverables = [team.deliverables[deliverable_name]]
-        else:
-            deliverables = team.deliverables.values()
-        for deliverable in deliverables:
-            for repository in deliverable.repositories.values():
-                if tags and not tags.issubset(repository.tags):
-                    continue
-                if code_only and not repository.code_related:
-                    continue
-                yield repository
