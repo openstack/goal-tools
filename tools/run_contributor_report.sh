e91b99fb27f5240a720b91269ca9facd4e604a94
Doug Hellmann <doug@doughellmann.com>
2018-05-01 15:05:22 -0400

    add script to run all of the reporting steps
    Start from a file with a query and end up with a file containing the
    basic summary report.
    
    Change-Id: I8193639bbfbbcb5d836e47f54f7b7ed7b397f67d
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ tools/run_contributor_report.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+if [ ! -f .tox/venv/bin/activate ]; then
+    tox -e venv --notest
+fi
+source .tox/venv/bin/activate
+
+for input in $*
+do
+    txt_file=${input%.qry}.txt
+    csv_file=${input%.qry}.csv
+    rpt_file=${input%.qry}.rpt
+    who-helped -v changes query "$(cat $input)" $txt_file
+    who-helped contributions list -f csv $txt_file | tee $csv_file
+    who-helped contributions summarize $csv_file | tee $rpt_file
+done
