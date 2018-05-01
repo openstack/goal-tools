6000cacd6f901e9c6344b61806e0cae9c40daf98
Doug Hellmann <doug@doughellmann.com>
2018-05-01 15:57:05 -0400

    verbose and debug mode in scripts
    Change-Id: Ic7a32039063288e120fde21cec58cdb5f04165dc
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- tools/run_contributor_report.sh
+++ tools/run_contributor_report.sh
@@ -10,7 +10,7 @@ do
     txt_file=${input%.qry}.txt
     csv_file=${input%.qry}.csv
     rpt_file=${input%.qry}.rpt
-    who-helped -v changes query "$(cat $input)" $txt_file
-    who-helped contributions list -f csv $txt_file | tee $csv_file
-    who-helped contributions summarize $csv_file | tee $rpt_file
+    who-helped -v --debug changes query "$(cat $input)" $txt_file
+    who-helped --debug contributions list -f csv $txt_file | tee $csv_file
+    who-helped --debug contributions summarize $csv_file | tee $rpt_file
 done
