8ebb1b805672ba58d60d0595bf2414a25681c93d
Doug Hellmann <doug@doughellmann.com>
2018-05-02 22:13:46 -0400

    skip the anonymous and distinct reports
    these are easy enough to produce from other data
    
    Change-Id: I7641b0c3320cf6f5d889f72c2eb6ab68ece6df84
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- tools/run_contributor_report.sh
+++ tools/run_contributor_report.sh
@@ -24,16 +24,6 @@ do
                --highlight-sponsors --ignore-single-vendor $dat_file \
         | tee $rpt_file.sponsor-contributions.csv
 
-    # CSV version of report summarizing contributions with orgs anonymized
-    who-helped contributions summarize -f csv \
-               --anon --ignore-single-vendor $dat_file \
-        | tee $rpt_file.anon-contributions.csv
-
-    # CSV version of distinct orgs report
-    who-helped contributions distinct -f csv \
-               --ignore-single-vendor $dat_file \
-        | tee $rpt_file.organizations.csv
-
     # CSV version of report summarizing people per org
     who-helped contributions summarize -f csv \
                --count Name --ignore-single-vendor $dat_file \
