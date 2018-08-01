3c6601d1f26f4cd288e1e11d7b1897d204804e86
Doug Hellmann <doug@doughellmann.com>
2018-08-01 09:39:36 -0400

    show the changes to be proposed before submitting the patches
    Change-Id: Ic85e947fdbf74b4799e92acda2f8e389fa97d232
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- tools/python3-first/propose.sh
+++ tools/python3-first/propose.sh
@@ -19,6 +19,13 @@ if [ -z "$team" ]; then
     exit 1
 fi
 
+LOGFILE="$workdir/$team-propose.txt"
+echo "Logging to $LOGFILE"
+# Set fd 1 and 2 to write the log file
+exec 1> >( tee "${LOGFILE}" ) 2>&1
+date
+echo $0 $@
+
 if [ ! -d .tox/venv ]; then
     tox -e venv --notest
 fi
@@ -29,6 +36,27 @@ BRANCHES="master ocata pike queens rocky"
 #set -x
 
 cd $workdir/$team
+
+for branch in $BRANCHES
+do
+    for repo in $(cat $branch)
+    do
+        echo
+        echo $repo $branch
+        if [ $branch = master ]; then
+            origin=origin/master
+        else
+            origin=origin/stable/$branch
+        fi
+        (cd $repo &&
+                git checkout python3-first-$branch &&
+                git log --oneline $origin..)
+    done
+done
+
+echo "Press return to continue"
+read ignoreme
+
 for branch in $BRANCHES
 do
     for repo in $(cat $branch)
