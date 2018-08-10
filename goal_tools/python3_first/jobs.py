5baf7e0b7d31d8dd2dfb5b3efdb7bffca25bc064
Andreas Jaeger <aj@suse.com>
2018-08-10 09:03:53 +0200

    Blacklist new translation job templates
    We introduce new translation templates, add them to the list of jobs
    to keep in project-config for python 3 goal.
    
    Depends-On: https://review.openstack.org/589372
    Change-Id: I99903317940b8612a10df4bc1d185a15e41af103

--- goal_tools/python3_first/jobs.py
+++ goal_tools/python3_first/jobs.py
@@ -22,6 +22,8 @@ LOG = logging.getLogger(__name__)
 # Items we know we need to keep in project-config.
 KEEP = set([
     'translation-jobs',
+    'translation-jobs-master-only',
+    'translation-jobs-master-stable',
     'translation-jobs-pike',
     'translation-jobs-queens',
     'translation-jobs-rocky',
