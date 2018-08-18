f6586c822e850cf799587b230baba4fd965943e9
Doug Hellmann <doug@doughellmann.com>
2018-08-18 10:39:30 -0400

    add script to process SIG repos
    Change-Id: I2d0215eae78bd0ddd70fa596cb6b49928985bce4
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ tools/python3-first/do_sig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+bindir=$(dirname $0)
+
+function usage {
+    echo "do_uc.sh WORKDIR"
+}
+
+workdir="$1"
+
+if [ -z "$workdir" ]; then
+    usage
+    exit 1
+fi
+
+REPOS="
+openstack/governance-sigs
+openstack/anchor
+openstack/ossa
+openstack/security-analysis
+openstack/security-doc
+openstack/security-specs
+openstack/syntribos
+openstack/syntribos-openstack-templates
+openstack/syntribos-payloads
+openstack/self-healing-sig
+"
+
+$bindir/do_team.sh "$workdir" "SIGs" $REPOS
