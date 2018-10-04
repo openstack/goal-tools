8bc009d8f7c5663116a4603c3a1a9e258e3778e4
Doug Hellmann <doug@doughellmann.com>
2018-10-04 07:08:38 -0400

    script to submit patches with wheel flag
    We may not need this after all, but check it in for posterity.
    
    Change-Id: Ie9568a3e56e1267ddd16b35d4cdcb782478b9906
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ tools/python3-first/propose-wheel.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+bindir=$(dirname $0)
+source $bindir/functions
+
+function usage {
+    echo "do_team.sh WORKDIR TEAM"
+}
+
+workdir=$1
+team="$2"
+
+if [ -z "$workdir" ]; then
+    usage
+    exit 1
+fi
+
+if [ -z "$team" ]; then
+    usage
+    exit 1
+fi
+
+out_dir=$(get_team_dir "$workdir" "$team")
+
+log_output "$out_dir" propose
+
+enable_tox
+
+function list_changes {
+    origin=origin/master
+    for repo in $(cat master)
+    do
+        (cd $repo &&
+                git log --oneline --pretty=format:"%h %s $repo $branch%n" $origin..)
+    done
+}
+
+cd "$out_dir"
+
+echo
+list_changes
+
+nchanges=$(list_changes 2>/dev/null | grep -v "^$" | wc -l)
+
+echo
+echo "About to propose $nchanges changes"
+
+echo
+echo "Press enter to continue"
+read ignoreme
+
+branches="master $(list_stable_branches $repo)"
+
+target=master
+
+for repo in $(cat $target)
+do
+    echo
+    echo $repo $branch
+    (cd $repo &&
+            git review -y -t python3-first $target)
+done
